835bb0e4d8cca5dab64e25f20400bc5e6a102aa6
Melle de Vries <melle@openweb.nl>
2020-12-05 15:27:00 +0100

    Cleanup

--- src/day1.rs
+++ src/day1.rs
@@ -1,18 +1,21 @@
-use std::fs::File;
-use std::io::{BufReader, BufRead};
+use super::utils;
+
+const INPUT_FILE: &str = "./resources/dayOne.txt";
 
 pub fn part_one() -> u32 {
-    find_sum_2020(read_from_file())
+    let numbers = utils::read_numbers_from_file(INPUT_FILE);
+    find_sum_2020(numbers)
 }
 
 pub fn part_two() -> u32 {
-    find_sum_2020_recursive(read_from_file(), 3)
+    let numbers = utils::read_numbers_from_file(INPUT_FILE);
+    find_sum_2020_recursive(numbers, 3)
 }
 
 fn find_sum_2020_recursive(numbers: Vec<u32>, size: usize) -> u32 {
     let mut current: Vec<u32> = Vec::new();
     let mut combinations: Vec<Vec<u32>> = Vec::new();
-    combinations_of_size(numbers, &mut current, &mut combinations, size);
+    utils::find_combinations(numbers, &mut current, &mut combinations, size);
 
     for combination in combinations {
         let sum: u32 = combination.iter().sum();
@@ -23,24 +26,6 @@ fn find_sum_2020_recursive(numbers: Vec<u32>, size: usize) -> u32 {
     0
 }
 
-fn combinations_of_size(numbers: Vec<u32>,
-                        current: &mut Vec<u32>,
-                        combinations: &mut Vec<Vec<u32>>,
-                        size: usize) {
-    for index in 0..numbers.len() {
-        current.push(numbers[index]);
-        let mut available_numbers = numbers.clone();
-        available_numbers.remove(index);
-
-        if current.len() == size {
-            combinations.push(current.clone());
-            current.remove(size - 1);
-            continue;
-        }
-        combinations_of_size(available_numbers, current, combinations, size);
-        current.remove(current.len() - 1);
-    }
-}
 
 fn find_sum_2020(mut numbers: Vec<u32>) -> u32 {
     numbers.sort();
@@ -56,26 +41,6 @@ fn find_sum_2020(mut numbers: Vec<u32>) -> u32 {
     panic!("Could not find the two numbers with sum 2020!")
 }
 
-fn read_from_file() -> Vec<u32> {
-    let file = match File::open("./resources/dayOne.txt") {
-        Ok(f) => f,
-        Err(e) => panic!(e)
-    };
-
-    let reader = BufReader::new(&file);
-    let mut numbers: Vec<u32> = Vec::new();
-    for line in reader.lines() {
-        let nr_string = line.unwrap();
-        println!("{}", nr_string);
-        let nr = match nr_string.trim().parse() {
-            Ok(num) => num,
-            Err(e) => panic!(e)
-        };
-        numbers.push(nr)
-    }
-    numbers
-}
-
 fn get_test_input() -> Vec<u32> {
     vec![1721, 979, 366, 299, 675, 1456]
 }
@@ -84,28 +49,4 @@ fn get_test_input() -> Vec<u32> {
 fn test_run_part_2() {
     let answer = find_sum_2020_recursive(get_test_input(), 3);
     assert_eq!(answer, 241861950);
-}
-
-#[test]
-fn combinations_have_correct_size() {
-    let input = get_test_input();
-    let mut current: Vec<u32> = Vec::new();
-    let mut combinations: Vec<Vec<u32>> = Vec::new();
-    combinations_of_size(input, &mut current, &mut combinations, 3);
-
-    for combination in combinations {
-        println!("{:?}", combination);
-        assert_eq!(combination.len(), 3);
-    }
-}
-
-#[test]
-fn all_combinations_are_found() {
-    // Currently broken: result will contain duplicates (also explains shitty performance)
-    let input = get_test_input();
-    let mut current: Vec<u32> = Vec::new();
-    let mut combinations: Vec<Vec<u32>> = Vec::new();
-    combinations_of_size(input, &mut current, &mut combinations, 3);
-
-    assert_eq!(combinations.len(), 20);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,15 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
 pub fn find_combinations(numbers: Vec<u32>,
-                     current: &mut Vec<u32>,
-                     combinations: &mut Vec<Vec<u32>>,
-                     size: usize) {
+                         current: &mut Vec<u32>,
+                         combinations: &mut Vec<Vec<u32>>,
+                         size: usize) {
 
     for index in 0..numbers.len() {
         let mut available_numbers = numbers.clone();
         current.push(available_numbers[index]);
-        available_numbers.drain(0..index);
+        available_numbers.drain(0..index + 1);
 
         if current.len() == size {
             combinations.push(current.clone());
@@ -18,6 +21,21 @@ pub fn find_combinations(numbers: Vec<u32>,
     }
 }
 
+pub fn read_file_lines(path: &str) -> Vec<String> {
+    if let Ok(file) = File::open(path) {
+        return BufReader::new(file).lines()
+            .map(|line| line.unwrap())
+            .collect();
+    }
+    Vec::new()
+}
+
+pub fn read_numbers_from_file(path: &str) -> Vec<u32> {
+    read_file_lines(path).iter()
+        .filter_map(|num| num.trim().parse().ok())
+        .collect()
+}
+
 #[test]
 fn all_combinations_found() {
     let input = vec![1721, 979, 366, 299, 675, 1456];
@@ -26,4 +44,29 @@ fn all_combinations_found() {
     find_combinations(input, &mut current, &mut combinations, 3);
 
     assert_eq!(combinations.len(), 20);
+}
+
+#[test]
+fn combinations_have_correct_size() {
+    let input = vec![1721, 979, 366, 299, 675, 1456];
+    let mut current: Vec<u32> = Vec::new();
+    let mut combinations: Vec<Vec<u32>> = Vec::new();
+    find_combinations(input, &mut current, &mut combinations, 3);
+
+    for combination in combinations {
+        println!("{:?}", combination);
+        assert_eq!(combination.len(), 3);
+    }
+}
+
+#[test]
+fn all_lines_are_read_from_file() {
+    let lines = read_file_lines("./resources/dayOne.txt");
+    assert_eq!(lines.len(), 200);
+}
+
+#[test]
+fn all_numbers_in_file_are_parsed() {
+    let total: u32 = read_numbers_from_file("./resources/dayOne.txt").iter().sum();
+    assert_eq!(total, 321845)
 }
